26c66f47af922e1e04ba00bd17c796f03bfa8095
matthias <294942509@qq.com>
2022-03-28 21:55:53 +0800

    2 commit

---
+++ cloud2020/cloud-stream-rabbitmq-consumer8802/src/main/java/com/matthias/springcloud/controller/ReceiveMessageController.java
@@ -0,0 +1,26 @@
+package com.matthias.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableBinding(Sink.class)
+@Slf4j
+public class ReceiveMessageController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message) {
+
+        log.info("*********server port=" + serverPort + "\t" + message.getPayload());
+    }
+
+}
