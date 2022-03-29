9e03bca0fe60f621d7e06e906bda770faeb5d481
matthias <294942509@qq.com>
2022-03-29 20:39:45 +0800

    nacos commit

---
+++ cloud2020/cloudalibaba-consumer-nacos-order83/src/main/java/com/matthias/springcloud/controller/OrderNacosController.java
@@ -0,0 +1,31 @@
+package com.matthias.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+@RequestMapping( "consumer")
+public class OrderNacosController {
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @Value("${service-url.nacos-user-service}")
+    private String serverUrl;
+
+    @GetMapping("/payment/nacos/{id}")
+    public String paymentInfo(@PathVariable("id") String id) {
+        return restTemplate.getForObject(serverUrl + "/payment/nacos/" + id, String.class);
+    }
+
+
+}
